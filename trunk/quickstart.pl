b2f2159d96decde66807c13ad08cc9809466b31c
penguintutor@gmail.com <penguintutor@gmail.com@38254c0d-b71d-0410-b7fa-0127e94c7c89>
2011-08-17 18:55:39 +0000

    Added quickstart

--- trunk/quickstart.pl
+++ trunk/quickstart.pl
@@ -1,27 +1,28 @@
 #!/usr/bin/perl -w
-# offline quiz - ask which quiz
+# Request personal details and select which quiz
 
 use CGI qw(:standard);
+use Quizlib::Misc;
 use Quizlib::Security;
 use Quizlib::Errors;
-use Quizlib::Misc;
 
 use strict;
 
-my $template = "templates/offline.html";
-my $page = "offline.pl";
+my $template = "templates/quickstart.html";
+my $page = "index.pl";
 
-our (%quiznames, @category, %numquestions, @csscategories, %cssindex, $offlinequiz, $headerfile, $footerfile);
+our (%quiznames, @category, %numquestions, @csscategories, %cssfile, %cssextra, %cssindex, $headerfile, $footerfile);
 # This is the one thing we don't error handle properly - if this fails then we can't find who to email etc.
 do "quiz.cfg" or die "Error loading quiz.cfg";
 
-# First check if offlinequiz is allowed
-if (!defined($offlinequiz) || !$offlinequiz) {Quizlib::Errors::offline_not_allowed();}
-
-# Allow alternate CSS using the style param - don't use here - but maintain for future use
+# Allow alternate CSS using the style param
 my $given_style = param ("style");
 if (!defined $given_style || $given_style eq "") {$given_style=$csscategories[0];}
 my $style = Quizlib::Security::chk_from_list ($page, "style", $given_style, @csscategories);
+
+my $css = $cssfile{$style};
+my $cssextraentry = $cssextra{$style};
+my $urlextra = "&amp;style=$style";
 
 # Create an option menu for choice of quiz's
 my $i;
@@ -30,8 +31,8 @@ for ($i=0; $i < scalar @category; $i++)
 	{
 	# Check that category & numquestions also exist else error
 	if (!defined $quiznames{$category[$i]} || !defined $numquestions{$category[$i]}) {Quizlib::Errors::config_error($page, "\%quiznames, or \%numquestions does not exist for $category[$i]");}
-	
-	$optionmenu .= "<option value=\"$category[$i]\">$quiznames{$category[$i]}</option>";
+	# note a space is added to ensure that a small letter (e.g. 1) isn't at the edge of the menu
+	$optionmenu .= "<option value=\"$category[$i]\">$quiznames{$category[$i]}&nbsp;</option>";
 	}
 	
 # Get headers and footers from external file
@@ -47,15 +48,21 @@ if (defined $footerfile && $footerfile ne "")
 }
 
 
+
 open(TEMPLATE, $template) or Quizlib::Errors::fileopen_error($page, "$template", "read"); 
 print header();
 while (<TEMPLATE>)
 	{
+	s/\%\%css\%\%/$css/g;
+	s/\%\%cssextra\%\%/$cssextraentry/g;
+	s/\%\%cssurl\%\%/$style/g;
 	s/\%\%footer\%\%/$footertext/g;
 	s/\%\%header\%\%/$headertext/g;
 	s/\%\%index\%\%/$cssindex{$style}/g;
 	s/\%\%quiznames\%\%/$optionmenu/;
-	s/\%\%style\%\%/$style/g;
+	s/\%\%urlextra\%\%/$urlextra/g;
+	
+	
 	
 	print;
 	}
