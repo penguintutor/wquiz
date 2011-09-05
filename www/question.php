a335334789882480cdc53c23dc799a7342b3b264
penguintutor@gmail.com <penguintutor@gmail.com@38254c0d-b71d-0410-b7fa-0127e94c7c89>
2011-09-05 16:34:03 +0000

    Further work on questions.php - add question number handling (basic) and added navigation bar - no handling of different actions yet.

--- www/question.php
+++ www/question.php
@@ -7,6 +7,7 @@ $GLOBALS['debug'] = 1;
 
 
 require_once("includes/setup.php");
+require_once("includes/questionNavigation.php");	// used later for navigation buttons
 
 // get the list of questions and current status
 $quiz_info = $quiz_session->getSessionInfo();
@@ -21,17 +22,54 @@ if (!isset($quiz_info['status'])||!is_int($quiz_info['status']))
 	header("Location: ".INDEX_FILE);
 }
 
+// Get the list of question numbers and current answers from session
+$questions_array = $quiz_session->getQuestions();
+// If we don't have questions then we error
+if (count($questions_array)<1) 
+{
+	$err = Errors::getInstance();
+	$err->errorEvent(ERROR_SESSION, "Session does not have any questions defined");
+}
+else {$num_questions = count($questions_array);}
+$answers_array = $quiz_session->getAnswers();
+
+
 // get question number from the post
-//if (!isset($_POST['questionnum']) || !is_int($_POST['questionnum'])) {$questionnum = 1;}
-//-here
+// question number is from 1 upwards - not 0 as the session array does
+// Check that this is a number and that it is within the session questions - otherwise we default to 1st question
+if (!isset($_POST['questionnum']) || !is_int($_POST['questionnum']) || $_POST['questionnum'] < 1) {$questionnum = 1;}
+elseif ($_POST['questionnum'] > $num_questions) {$questionnum = $num_questions;}
+else {$questionnum = $_POST['questionnum'];}
+
+
+// -here Save answer if changed
+
+
+// -here Handle change in page (eg. Finish / trying to go past first) 
 
 
 // Pull in templates
 $templates->includeTemplate('header', 'normal');
-$question = new Question(0, $qdb->getQuestion(2));
+
+// start form
+// Form starts at the top
+print "<form id=\"wquiz-form\" method=\"post\" action=\"question.php\">\n";
+
+// load this question - note -1 used to select array position (ie. question 1 = array 0)
+$question = new Question(0, $qdb->getQuestion($questions_array[$questionnum-1]));
 // first print status bar if req'd (eg. question 1 of 10)
 // answer is currently selected -1 = not answered
 print ($question->getHtmlString(-1));
+
+
+// add navigation buttons
+print "<div id=\"".CSS_ID_NAVIGATION."\">\n";
+showNavigation($questionnum, 1, $num_questions);
+print "\n</div><!-- ".CSS_ID_NAVIGATION." -->\n";
+
+//--here end form
+print "</form>\n";
+
 
 // footer templates
 $templates->includeTemplate('footer', 'normal');
