28d13c8b3062d68bbe130a12ff3919962b777e7d
penguintutor@gmail.com <penguintutor@gmail.com@38254c0d-b71d-0410-b7fa-0127e94c7c89>
2011-09-23 16:05:54 +0000

    Start work on admin functions

---
+++ www/admin/adminindex.php
@@ -0,0 +1,60 @@
+<?php
+
+// Enable debugging
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+
+//$debug = true;
+
+// message is used to provide feedback to the user
+//eg. if we get here from an expired session
+$message = '';
+
+require_once("../includes/setup.php");
+// Authentication class required for admin functions
+require_once("../includes/SimpleAuth.php");
+// add this here as not required for some pages (which use Quiz.php instead)
+require_once ($include_dir."Quizzes.php");
+
+
+/*** Authentication ***/
+// user must be logged in for any admin functions
+// this needs to be before we output anything as it uses sessions (cookies)
+$auth = new SimpleAuth ($settings->getSetting('admin_login_username'), $settings->getSetting('admin_login_password'), $settings->getSetting('admin_login_expirytime'));
+// if not logged in redirect to login page
+$status = $auth->checkLogin();
+if ($status != 1) 
+	{
+	// no from as use default which goes back to this page
+	header("Location: ".ADMIN_FILE."?status=$status");
+	// header will redirect to a new page so we just close this script
+	exit (0);	//Important to stop script here
+	}
+// If we reach here then login was successful
+$sessionUsername = $auth->getUser();
+
+
+
+
+// header template
+$templates->includeTemplate('header', 'admin');
+
+$questions = 
+print <<< EOT
+<h1>Administration</h1>
+<h2>Tasks</h2>
+<ul><li>Quizzes (not yet implemented)</li>
+<li><a href="$questions">Questions</a></li>
+</ul>
+
+EOT;
+
+
+
+// footer template
+$templates->includeTemplate('footer', 'admin');
+
+
+
+?>
